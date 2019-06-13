b294be7e9bbc688b40720643468a9eb4aebad82d
AnnoyingTeemo <m.j.wighton@student.saintkentigern.com>
2019-06-14 10:18:38 +1200

    Added Cart and food types buttons

--- 2.7-Assesment/Test UI.py
+++ 2.7-Assesment/Test UI.py
@@ -6,33 +6,47 @@ class PegGameWindow(QtWidgets.QWidget):
         QtWidgets.QWidget.__init__(self)
         self.setup()
     def setup(self):
-        self.setGeometry(0, 0, 1700, 800)
+        self.setGeometry(0, 0, 1500, 850)
         self.setWindowTitle('Test UI')
         #Buttons code goes here
         self.Fruits = QtWidgets.QPushButton("Fruits", self)
-        self.Fruits.setMinimumSize(100,100)
-        self.Fruits.move(10, 10)
+        self.Fruits.setMinimumSize(130,130)
+        self.Fruits.move(30, 10)
 
         self.Vegetables = QtWidgets.QPushButton("Vegetables", self)
-        self.Vegetables.setMinimumSize(100,100)
-        self.Vegetables.move(160, 10)
+        self.Vegetables.setMinimumSize(130,130)
+        self.Vegetables.move(190, 10)
 
         self.MilkProducts = QtWidgets.QPushButton("Milk Products", self)
-        self.MilkProducts.setMinimumSize(100,100)
-        self.MilkProducts.move(310, 10)
+        self.MilkProducts.setMinimumSize(130,130)
+        self.MilkProducts.move(350, 10)
 
         self.Nuts = QtWidgets.QPushButton("Nuts", self)
-        self.Nuts.setMinimumSize(100,100)
-        self.Nuts.move(460, 10)
+        self.Nuts.setMinimumSize(130,130)
+        self.Nuts.move(510, 10)
 
         self.Jams = QtWidgets.QPushButton("Jams", self)
-        self.Jams.setMinimumSize(100,100)
-        self.Jams.move(610, 10)
+        self.Jams.setMinimumSize(130,130)
+        self.Jams.move(670, 10)
 
         self.Juices = QtWidgets.QPushButton("Juices", self)
-        self.Juices.setMinimumSize(100,100)
-        self.Juices.move(760, 10)
+        self.Juices.setMinimumSize(130,130)
+        self.Juices.move(830, 10)
 
+        #Checkout Code
+        self.Cart = QtWidgets.QTextBrowser(self)
+        self.Cart.append("Test")
+        self.Cart.append("Test2")
+        self.Cart.setMinimumSize(450,650)
+        self.Cart.move(1000,10)
+
+        self.Checkout = QtWidgets.QPushButton("Checkout", self)
+        self.Checkout.setMinimumSize(220,130)
+        self.Checkout.move(1230,670)
+
+        self.Clear = QtWidgets.QPushButton("Clear Cart", self)
+        self.Clear.setMinimumSize(220,130)
+        self.Clear.move(1000,670)
 
         self.show()
 if __name__ == "__main__":
@@ -40,4 +54,5 @@ if __name__ == "__main__":
  main_window = PegGameWindow()
  app.exec_()
 
-
+# 150, ~, 1150
+# the wooden one with glass windows
