b36ac642a54e15cb3414460518a3904891ff9349
AnnoyingTeemo <m.j.wighton@student.saintkentigern.com>
2019-06-17 10:22:16 +1200

    Its worse

--- 2.7-Assesment/Test UI code combo 1.py
+++ 2.7-Assesment/Test UI code combo 1.py
@@ -108,7 +108,7 @@ class TestUI(QtWidgets.QWidget):
         self.Food0.move(100,500)
         self.Food0.setObjectName("Food0")
         self.Food0.hide()
-        self.Food0.clicked.connect(self.Food1Clicked)
+        self.Food0.clicked.connect(self.Food0Clicked)
 
         self.Food1 = QtWidgets.QPushButton(testDictionary["Food 1"]["Name"], self)
         self.Food1.setMinimumSize(130,130)
@@ -122,7 +122,7 @@ class TestUI(QtWidgets.QWidget):
         self.Food2.setObjectName("Food2")
         self.Food2.move(300,500)
         self.Food2.hide()
-        self.Food2.clicked.connect(self.Food1Clicked)
+        self.Food2.clicked.connect(self.Food2Clicked)
 
 
         self.Food3 = QtWidgets.QPushButton(testDictionary["Food 3"]["Name"], self)
@@ -130,57 +130,67 @@ class TestUI(QtWidgets.QWidget):
         self.Food3.move(400,500)
         self.Food3.setObjectName("Food3")
         self.Food3.hide()
-        self.Food3.clicked.connect(self.Food1Clicked)
+        self.Food3.clicked.connect(self.Food3Clicked)
 
         self.Food4 = QtWidgets.QPushButton(testDictionary["Food 4"]["Name"], self)
         self.Food4.setMinimumSize(130,130)
         self.Food4.move(500,500)
         self.Food4.setObjectName("Food4")
         self.Food4.hide()
-        self.Food4.clicked.connect(self.Food1Clicked)
+        self.Food4.clicked.connect(self.Food4Clicked)
 
         self.Food5 = QtWidgets.QPushButton(testDictionary["Food 5"]["Name"], self)
         self.Food5.setMinimumSize(130,130)
         self.Food5.setObjectName("Food5")
         self.Food5.move(600,500)
         self.Food5.hide()
-        self.Food5.clicked.connect(self.Food1Clicked)
+        self.Food5.clicked.connect(self.Food5Clicked)
 
         self.Food6 = QtWidgets.QPushButton(testDictionary["Food 6"]["Name"], self)
         self.Food6.setMinimumSize(130,130)
         self.Food6.setObjectName("Food6")
         self.Food6.move(700,500)
         self.Food6.hide()
-        self.Food6.clicked.connect(self.Food1Clicked)
+        self.Food6.clicked.connect(self.Food6Clicked)
 
         self.Food7 = QtWidgets.QPushButton(testDictionary["Food 7"]["Name"], self)
         self.Food7.setMinimumSize(130,130)
         self.Food7.setObjectName("Food7")
         self.Food7.move(800,500)
         self.Food7.hide()
-        self.Food7.clicked.connect(self.Food1Clicked)
+        self.Food7.clicked.connect(self.Food7Clicked)
 
         self.show()
-    #Code for buttons
+    #Code for buttons warning terrible code ahead
+
+
     def Veges_Clicked(self):
         for item in VegiesList: #I need to find a better way to do this than 7 if statements
             #Temp solution i swear
             if self.Food0.objectName() == item.replace(' ', ''):
                 self.Food0.show()
+            else: self.Food0.hide()
             if self.Food1.objectName() == item.replace(' ', ''):
                 self.Food1.show()
+            else: self.Food1.hide()
             if self.Food2.objectName() == item.replace(' ', ''):
                 self.Food2.show()
+            else: self.Food2.hide()
             if self.Food3.objectName() == item.replace(' ', ''):
                 self.Food3.show()
+            else: self.Food3.hide()
             if self.Food4.objectName() == item.replace(' ', ''):
                 self.Food4.show()
+            else: self.Food4.hide()
             if self.Food5.objectName() == item.replace(' ', ''):
                 self.Food5.show()
+            else: self.Food5.hide()
             if self.Food6.objectName() == item.replace(' ', ''):
                 self.Food6.show()
+            else: self.Food6.hide()
             if self.Food7.objectName() == item.replace(' ', ''):
                 self.Food7.show()
+            else: self.Food7.hide()
 
 
 
@@ -193,20 +203,29 @@ class TestUI(QtWidgets.QWidget):
             #Temp solution i swear
             if self.Food0.objectName() == item.replace(' ', ''):
                 self.Food0.show()
+            else: self.Food0.hide()
             if self.Food1.objectName() == item.replace(' ', ''):
                 self.Food1.show()
+            else: self.Food1.hide()
             if self.Food2.objectName() == item.replace(' ', ''):
                 self.Food2.show()
+            else: self.Food2.hide()
             if self.Food3.objectName() == item.replace(' ', ''):
                 self.Food3.show()
+            else: self.Food3.hide()
             if self.Food4.objectName() == item.replace(' ', ''):
                 self.Food4.show()
+            else: self.Food4.hide()
             if self.Food5.objectName() == item.replace(' ', ''):
                 self.Food5.show()
+            else: self.Food5.hide()
             if self.Food6.objectName() == item.replace(' ', ''):
                 self.Food6.show()
+            else: self.Food6.hide()
             if self.Food7.objectName() == item.replace(' ', ''):
                 self.Food7.show()
+            else: self.Food7.hide()
+
 
 
 
@@ -219,20 +238,29 @@ class TestUI(QtWidgets.QWidget):
             #Temp solution i swear
             if self.Food0.objectName() == item.replace(' ', ''):
                 self.Food0.show()
+            else: self.Food0.hide()
             if self.Food1.objectName() == item.replace(' ', ''):
                 self.Food1.show()
+            else: self.Food1.hide()
             if self.Food2.objectName() == item.replace(' ', ''):
                 self.Food2.show()
+            else: self.Food2.hide()
             if self.Food3.objectName() == item.replace(' ', ''):
                 self.Food3.show()
+            else: self.Food3.hide()
             if self.Food4.objectName() == item.replace(' ', ''):
                 self.Food4.show()
+            else: self.Food4.hide()
             if self.Food5.objectName() == item.replace(' ', ''):
                 self.Food5.show()
+            else: self.Food5.hide()
             if self.Food6.objectName() == item.replace(' ', ''):
                 self.Food6.show()
+            else: self.Food6.hide()
             if self.Food7.objectName() == item.replace(' ', ''):
                 self.Food7.show()
+            else: self.Food7.hide()
+
 
 
 
@@ -245,20 +273,28 @@ class TestUI(QtWidgets.QWidget):
             #Temp solution i swear
             if self.Food0.objectName() == item.replace(' ', ''):
                 self.Food0.show()
+            else: self.Food0.hide()
             if self.Food1.objectName() == item.replace(' ', ''):
                 self.Food1.show()
+            else: self.Food1.hide()
             if self.Food2.objectName() == item.replace(' ', ''):
                 self.Food2.show()
+            else: self.Food2.hide()
             if self.Food3.objectName() == item.replace(' ', ''):
                 self.Food3.show()
+            else: self.Food3.hide()
             if self.Food4.objectName() == item.replace(' ', ''):
                 self.Food4.show()
+            else: self.Food4.hide()
             if self.Food5.objectName() == item.replace(' ', ''):
                 self.Food5.show()
+            else: self.Food5.hide()
             if self.Food6.objectName() == item.replace(' ', ''):
                 self.Food6.show()
+            else: self.Food6.hide()
             if self.Food7.objectName() == item.replace(' ', ''):
                 self.Food7.show()
+            else: self.Food7.hide()
 
 
 
@@ -271,20 +307,29 @@ class TestUI(QtWidgets.QWidget):
             #Temp solution i swear
             if self.Food0.objectName() == item.replace(' ', ''):
                 self.Food0.show()
+            else: self.Food0.hide()
             if self.Food1.objectName() == item.replace(' ', ''):
                 self.Food1.show()
+            else: self.Food1.hide()
             if self.Food2.objectName() == item.replace(' ', ''):
                 self.Food2.show()
+            else: self.Food2.hide()
             if self.Food3.objectName() == item.replace(' ', ''):
                 self.Food3.show()
+            else: self.Food3.hide()
             if self.Food4.objectName() == item.replace(' ', ''):
                 self.Food4.show()
+            else: self.Food4.hide()
             if self.Food5.objectName() == item.replace(' ', ''):
                 self.Food5.show()
+            else: self.Food5.hide()
             if self.Food6.objectName() == item.replace(' ', ''):
                 self.Food6.show()
+            else: self.Food6.hide()
             if self.Food7.objectName() == item.replace(' ', ''):
                 self.Food7.show()
+            else: self.Food7.hide()
+
 
 
 
@@ -297,20 +342,29 @@ class TestUI(QtWidgets.QWidget):
             #Temp solution i swear
             if self.Food0.objectName() == item.replace(' ', ''):
                 self.Food0.show()
+            else: self.Food0.hide()
             if self.Food1.objectName() == item.replace(' ', ''):
                 self.Food1.show()
+            else: self.Food1.hide()
             if self.Food2.objectName() == item.replace(' ', ''):
                 self.Food2.show()
+            else: self.Food2.hide()
             if self.Food3.objectName() == item.replace(' ', ''):
                 self.Food3.show()
+            else: self.Food3.hide()
             if self.Food4.objectName() == item.replace(' ', ''):
                 self.Food4.show()
+            else: self.Food4.hide()
             if self.Food5.objectName() == item.replace(' ', ''):
                 self.Food5.show()
+            else: self.Food5.hide()
             if self.Food6.objectName() == item.replace(' ', ''):
                 self.Food6.show()
+            else: self.Food6.hide()
             if self.Food7.objectName() == item.replace(' ', ''):
                 self.Food7.show()
+            else: self.Food7.hide()
+
 
 
 
@@ -318,8 +372,22 @@ class TestUI(QtWidgets.QWidget):
         #self.Food1.show()
         #self.Cart.append(random.randint(0, 10000)) This crashed it
         self.show()
+    def Food0Clicked(self):
+        self.Cart.append(f"{testDictionary['Food 0']['Name']}: {testDictionary['Food 0']['Price']}")
     def Food1Clicked(self):
         self.Cart.append(f"{testDictionary['Food 1']['Name']}: {testDictionary['Food 1']['Price']}")
+    def Food2Clicked(self):
+        self.Cart.append(f"{testDictionary['Food 2']['Name']}: {testDictionary['Food 2']['Price']}")
+    def Food3Clicked(self):
+        self.Cart.append(f"{testDictionary['Food 3']['Name']}: {testDictionary['Food 3']['Price']}")
+    def Food4Clicked(self):
+        self.Cart.append(f"{testDictionary['Food 4']['Name']}: {testDictionary['Food 4']['Price']}")
+    def Food5Clicked(self):
+        self.Cart.append(f"{testDictionary['Food 5']['Name']}: {testDictionary['Food 5']['Price']}")
+    def Food6Clicked(self):
+        self.Cart.append(f"{testDictionary['Food 6']['Name']}: {testDictionary['Food 6']['Price']}")
+    def Food7Clicked(self):
+        self.Cart.append(f"{testDictionary['Food 7']['Name']}: {testDictionary['Food 7']['Price']}")
     def Clear_Clicked(self):
         self.Cart.setText("")
 if __name__ == "__main__":
