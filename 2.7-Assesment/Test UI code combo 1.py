b82118552fc3f156cb6a47661967ace2123a2938
AnnoyingTeemo <m.j.wighton@student.saintkentigern.com>
2019-06-19 11:56:24 +1200

    shortened code

--- 2.7-Assesment/Test UI code combo 1.py
+++ 2.7-Assesment/Test UI code combo 1.py
@@ -193,19 +193,21 @@ class TestUI(QtWidgets.QWidget):
         self.Food0.move(30,200)
         self.Food0.setObjectName("Food 0")
         self.Food0.hide()
-        #self.Food0.clicked.connect(self.Food0Clicked)
+        self.Food0.clicked.connect(self.Food0Clicked)
         self.Food1 = QtWidgets.QPushButton("Food1", self)
         self.Food1.setMinimumSize(130,130)
         self.Food1.move(190,200)
         self.Food1.setObjectName("Food 1")
         self.Food1.hide()
-        self.Food1.clicked.connect(self.Food1Clicked)
+        #self.Food1.clicked.connect(self.Food1Clicked)
+
         self.Food2 = QtWidgets.QPushButton("Food2", self)
         self.Food2.setMinimumSize(130,130)
         self.Food2.move(350,200)
         self.Food2.setObjectName("Food 2")
         self.Food2.hide()
-       # self.Food2.clicked.connect(self.Food2Clicked)
+       #self.Food2.clicked.connect(self.Food2Clicked)
+
         self.Food3 = QtWidgets.QPushButton("Food3", self)
         self.Food3.setMinimumSize(130,130)
         self.Food3.move(510,200)
@@ -269,102 +271,12 @@ class TestUI(QtWidgets.QWidget):
 
 
     def Veges_Clicked(self):
-        #I need to find a better way to do this than 7 if statements
-        #Temp solution i swear
-
+        for i in range(len(self.FoodsDictionary)):
+            self.FoodsDictionary[i].hide()
         for i in range(len(VegiesList)):
             self.FoodsDictionary[i].setText(testDictionary["Vegetable"][VegiesList[i]]["Name"])
             self.FoodsDictionary[i].show()
-            print(i)
-            print(self.FoodsDictionary[i].objectName())
-            print(self.FoodsDictionary[i].pos())
-        #
-        # try:
-        #     self.Food0.setText(testDictionary["Vegetable"][VegiesList[0]]["Name"])
-        #     self.Food0.show()
-        # except KeyError:
-        #     pass
-        # try:
-        #     self.Food1.setText(testDictionary["Vegetable"][VegiesList[1]]["Name"])
-        #     self.Food1.show()
-        # except KeyError:
-        #     pass
-        # try:
-        #     self.Food2.setText(testDictionary["Vegetable"][VegiesList[1]]["Name"])
-        #     self.Food2.show()
-        # except KeyError:
-        #     pass
-        # try:
-        #     self.Food3.setText(testDictionary["Vegetable"][VegiesList[1]]["Name"])
-        #     self.Food3.show()
-        # except KeyError:
-        #     pass
-        # try:
-        #     self.Food4.setText(testDictionary["Vegetable"][VegiesList[1]]["Name"])
-        #     self.Food4.show()
-        # except KeyError:
-        #     pass
-        # try:
-        #     self.Food5.setText(testDictionary["Vegetable"][VegiesList[1]]["Name"])
-        #     self.Food5.show()
-        # except KeyError:
-        #     pass
-        # try:
-        #     self.Food6.setText(testDictionary["Vegetable"][VegiesList[1]]["Name"])
-        #     self.Food6.show()
-        # except KeyError:
-        #     pass
-        # try:
-        #     self.Food7.setText(testDictionary["Vegetable"][VegiesList[1]]["Name"])
-        #     self.Food7.show()
-        # except KeyError:
-        #     pass
-        # try:
-        #     self.Food1.setText(testDictionary["Vegetable"][VegiesList[1]]["Name"])
-        #     self.Food1.show()
-        # except KeyError:
-        #     pass
-        # try:
-        #     self.Food1.setText(testDictionary["Vegetable"][VegiesList[1]]["Name"])
-        #     self.Food1.show()
-        # except KeyError:
-        #     pass
-        # try:
-        #     self.Food1.setText(testDictionary["Vegetable"][VegiesList[1]]["Name"])
-        #     self.Food1.show()
-        # except KeyError:
-        #     pass
-        # try:
-        #     self.Food1.setText(testDictionary["Vegetable"][VegiesList[1]]["Name"])
-        #     self.Food1.show()
-        # except KeyError:
-        #     pass
-        #
-        # if self.Food0.objectName() in VegiesList:
-        #     self.Food0.show()
-        # else: self.Food0.hide()
-        # if self.Food1.objectName() in VegiesList:
-        #     self.Food1.show()
-        # else: self.Food1.hide()
-        # if self.Food2.objectName() in VegiesList:
-        #     self.Food2.show()
-        # else: self.Food2.hide()
-        # if self.Food3.objectName() in VegiesList:
-        #     self.Food3.show()
-        # else: self.Food3.hide()
-        # if self.Food4.objectName() in VegiesList:
-        #     self.Food4.show()
-        # else: self.Food4.hide()
-        # if self.Food5.objectName() in VegiesList:
-        #     self.Food5.show()
-        # else: self.Food5.hide()
-        # if self.Food6.objectName() in VegiesList:
-        #     self.Food6.show()
-        # else: self.Food6.hide()
-        # if self.Food7.objectName() in VegiesList:
-        #     self.Food7.show()
-        # else: self.Food7.hide()
-
+            self.FoodsDictionary[i].setObjectName(testDictionary["Vegetable"][VegiesList[i]]["Name"])
 
 
         #self.Cart.append("This is a test")
@@ -372,184 +284,70 @@ class TestUI(QtWidgets.QWidget):
         #self.Cart.append(random.randint(0, 10000)) This crashed it
         self.show()
     def Milk_Clicked(self):
-        #I need to find a better way to do this than 7 if statements
-        #Temp solution i swear
-        if self.Food0.objectName() in MilkProductsList:
-            self.Food0.show()
-        else: self.Food0.hide()
-        if self.Food1.objectName() in MilkProductsList:
-            self.Food1.show()
-        else: self.Food1.hide()
-        if self.Food2.objectName() in MilkProductsList:
-            self.Food2.show()
-        else: self.Food2.hide()
-        if self.Food3.objectName() in MilkProductsList:
-            self.Food3.show()
-        else: self.Food3.hide()
-        if self.Food4.objectName() in MilkProductsList:
-            self.Food4.show()
-        else: self.Food4.hide()
-        if self.Food5.objectName() in MilkProductsList:
-            self.Food5.show()
-        else: self.Food5.hide()
-        if self.Food6.objectName() in MilkProductsList:
-            self.Food6.show()
-        else: self.Food6.hide()
-        if self.Food7.objectName() in MilkProductsList:
-            self.Food7.show()
-        else: self.Food7.hide()
 
-
-
-
+        for i in range(len(self.FoodsDictionary)):
+            self.FoodsDictionary[i].hide()
+        for i in range(len(MilkProductsList)):
+            self.FoodsDictionary[i].setText(testDictionary["Milk Product"][MilkProductsList[i]]["Name"])
+            self.FoodsDictionary[i].show()
+            self.FoodsDictionary[i].setObjectName(testDictionary["Milk Product"][MilkProductsList[i]]["Name"])
 
         #self.Cart.append("This is a test")
         #self.Food1.show()
         #self.Cart.append(random.randint(0, 10000)) This crashed it
         self.show()
     def Nuts_Clicked(self):
-        #I need to find a better way to do this than 7 if statements
-        #Temp solution i swear
-        if self.Food0.objectName() in NutsList:
-            self.Food0.show()
-        else: self.Food0.hide()
-        if self.Food1.objectName() in NutsList:
-            self.Food1.show()
-        else: self.Food1.hide()
-        if self.Food2.objectName() in NutsList:
-            self.Food2.show()
-        else: self.Food2.hide()
-        if self.Food3.objectName() in NutsList:
-            self.Food3.show()
-        else: self.Food3.hide()
-        if self.Food4.objectName() in NutsList:
-            self.Food4.show()
-        else: self.Food4.hide()
-        if self.Food5.objectName() in NutsList:
-            self.Food5.show()
-        else: self.Food5.hide()
-        if self.Food6.objectName() in NutsList:
-            self.Food6.show()
-        else: self.Food6.hide()
-        if self.Food7.objectName() in NutsList:
-            self.Food7.show()
-        else: self.Food7.hide()
-
-
-
+        for i in range(len(self.FoodsDictionary)):
+            self.FoodsDictionary[i].hide()
+        for i in range(len(NutsList)):
+            self.FoodsDictionary[i].setText(testDictionary["Nuts"][NutsList[i]]["Name"])
+            self.FoodsDictionary[i].show()
+            self.FoodsDictionary[i].setObjectName(testDictionary["Nuts"][NutsList[i]]["Name"])
 
         #self.Cart.append("This is a test")
         #self.Food1.show()
         #self.Cart.append(random.randint(0, 10000)) This crashed it
         self.show()
     def Jams_Clicked(self):
-        #I need to find a better way to do this than 7 if statements
-        #Temp solution i swear
-        if self.Food0.objectName() in JamsList:
-            self.Food0.show()
-        else: self.Food0.hide()
-        if self.Food1.objectName() in JamsList:
-            self.Food1.show()
-        else: self.Food1.hide()
-        if self.Food2.objectName() in JamsList:
-            self.Food2.show()
-        else: self.Food2.hide()
-        if self.Food3.objectName() in JamsList:
-            self.Food3.show()
-        else: self.Food3.hide()
-        if self.Food4.objectName() in JamsList:
-            self.Food4.show()
-        else: self.Food4.hide()
-        if self.Food5.objectName() in JamsList:
-            self.Food5.show()
-        else: self.Food5.hide()
-        if self.Food6.objectName() in JamsList:
-            self.Food6.show()
-        else: self.Food6.hide()
-        if self.Food7.objectName() in JamsList:
-            self.Food7.show()
-        else: self.Food7.hide()
-
-
+        for i in range(len(self.FoodsDictionary)):
+            self.FoodsDictionary[i].hide()
+        for i in range(len(JamsList)):
+            self.FoodsDictionary[i].setText(testDictionary["Jam"][JamsList[i]]["Name"])
+            self.FoodsDictionary[i].show()
+            self.FoodsDictionary[i].setObjectName(testDictionary["Jam"][JamsList[i]]["Name"])
 
         #self.Cart.append("This is a test")
         #self.Food1.show()
         #self.Cart.append(random.randint(0, 10000)) This crashed it
         self.show()
     def Juices_Clicked(self):
-        #I need to find a better way to do this than 7 if statements
-        #Temp solution i swear
-        if self.Food0.objectName() in JuicesList:
-            self.Food0.show()
-        else: self.Food0.hide()
-        if self.Food1.objectName() in JuicesList:
-            self.Food1.show()
-        else: self.Food1.hide()
-        if self.Food2.objectName() in JuicesList:
-            self.Food2.show()
-        else: self.Food2.hide()
-        if self.Food3.objectName() in JuicesList:
-            self.Food3.show()
-        else: self.Food3.hide()
-        if self.Food4.objectName() in JuicesList:
-            self.Food4.show()
-        else: self.Food4.hide()
-        if self.Food5.objectName() in JuicesList:
-            self.Food5.show()
-        else: self.Food5.hide()
-        if self.Food6.objectName() in JuicesList:
-            self.Food6.show()
-        else: self.Food6.hide()
-        if self.Food7.objectName() in JuicesList:
-            self.Food7.show()
-        else: self.Food7.hide()
-
-
-
+        for i in range(len(self.FoodsDictionary)):
+            self.FoodsDictionary[i].hide()
+        for i in range(len(JuicesList)):
+            self.FoodsDictionary[i].setText(testDictionary["Juice"][JuicesList[i]]["Name"])
+            self.FoodsDictionary[i].show()
+            self.FoodsDictionary[i].setObjectName(testDictionary["Juice"][JuicesList[i]]["Name"])
 
         #self.Cart.append("This is a test")
         #self.Food1.show()
         #self.Cart.append(random.randint(0, 10000)) This crashed it
         self.show()
     def Fruits_Clicked(self):
-        #I need to find a better way to do this than 7 if statements
-        #Temp solution i swear
-        if self.Food0.objectName() in FruitsList:
-            self.Food0.show()
-        else: self.Food0.hide()
-        if self.Food1.objectName() in FruitsList:
-            self.Food1.show()
-        else: self.Food1.hide()
-        if self.Food2.objectName() in FruitsList:
-            self.Food2.show()
-        else: self.Food2.hide()
-        if self.Food3.objectName() in FruitsList:
-            self.Food3.show()
-        else: self.Food3.hide()
-        if self.Food4.objectName() in FruitsList:
-            self.Food4.show()
-        else: self.Food4.hide()
-        if self.Food5.objectName() in FruitsList:
-            self.Food5.show()
-        else: self.Food5.hide()
-        if self.Food6.objectName() in FruitsList:
-            self.Food6.show()
-        else: self.Food6.hide()
-        if self.Food7.objectName() in FruitsList:
-            self.Food7.show()
-        else: self.Food7.hide()
-
-
-
+        for i in range(len(self.FoodsDictionary)):
+            self.FoodsDictionary[i].hide()
+        for i in range(len(FruitsList)):
+            self.FoodsDictionary[i].setText(testDictionary["Juice"][JuicesList[i]]["Name"])
+            self.FoodsDictionary[i].show()
+            self.FoodsDictionary[i].setObjectName(testDictionary["Juice"][JuicesList[i]]["Name"])
 
         #self.Cart.append("This is a test")
         #self.Food1.show()
         #self.Cart.append(random.randint(0, 10000)) This crashed it
         self.show()
     def Food0Clicked(self):
-        self.Cart.append(f"{testDictionary['Food 0']['Name']}: {testDictionary['Food 0']['Price']}")
-        self.currentOrders.append("Food 0")
-
+        #self.Cart.append(f"{testDictionary['Food 0']['Name']}: {testDictionary['Food 0']['Price']}")
+        #self.currentOrders.append()
+        self.Cart.append(self.Food0.objectName())
         total = 0
         for item in self.currentOrders:
             total += testDictionary[item]["RawPrice"]
