ecc541c01aac119f6f4212780e66f9fbf5df8fd8
AnnoyingTeemo <m.j.wighton@student.saintkentigern.com>
2019-06-19 14:16:44 +1200

    started making a better cart look

--- 2.7-Assesment/Test UI code combo 1.py
+++ 2.7-Assesment/Test UI code combo 1.py
@@ -40,8 +40,6 @@ for word in foods:
     i += 1
 print(testDictionary)
 
-# foods.close()
-
 for item in testDictionary["Fruit"]:
         if testDictionary["Fruit"][item]["Type of food"] == "Fruit":
             FruitsList.append(item)
@@ -60,20 +58,18 @@ for item in testDictionary["Jam"]:
 for item in testDictionary["Juice"]:
         if testDictionary["Juice"][item]["Type of food"] == "Juice":
             JuicesList.append(item)
-
 print(FruitsList)
 print(VegiesList)
 print(MilkProductsList)
 print(NutsList)
 print(JamsList)
 print(JuicesList)
-
 class TestUI(QtWidgets.QWidget):
     def __init__(self):
         QtWidgets.QWidget.__init__(self)
         self.setup()
     def setup(self):
-
+        self.CurrentCart = []
         self.CurrentFoodType = "Null"
         self.setGeometry(0, 0, 1500, 850)
         self.setWindowTitle('Test UI')
@@ -130,66 +126,6 @@ class TestUI(QtWidgets.QWidget):
         self.Clear.move(1000,670)
         self.Clear.clicked.connect(self.Clear_Clicked)
 
-        # #Add better buttons here, this is just for testing so far
-        # self.Food0 = QtWidgets.QPushButton(testDictionary["Food 0"]["Name"], self)
-        # self.Food0.setMinimumSize(130,130)
-        # self.Food0.move(100,500)
-        # self.Food0.setObjectName("Food 0")
-        # self.Food0.hide()
-        # self.Food0.clicked.connect(self.Food0Clicked)
-        #
-        # self.Food1 = QtWidgets.QPushButton(testDictionary["Food 1"]["Name"], self)
-        # self.Food1.setMinimumSize(130,130)
-        # self.Food1.setObjectName("Food 1")
-        # self.Food1.move(200,500)
-        # self.Food1.hide()
-        # self.Food1.clicked.connect(self.Food1Clicked)
-        #
-        # self.Food2 = QtWidgets.QPushButton(testDictionary["Food 2"]["Name"], self)
-        # self.Food2.setMinimumSize(130,130)
-        # self.Food2.setObjectName("Food 2")
-        # self.Food2.move(300,500)
-        # self.Food2.hide()
-        # self.Food2.clicked.connect(self.Food2Clicked)
-        #
-        #
-        # self.Food3 = QtWidgets.QPushButton(testDictionary["Food 3"]["Name"], self)
-        # self.Food3.setMinimumSize(130,130)
-        # self.Food3.move(400,500)
-        # self.Food3.setObjectName("Food 3")
-        # self.Food3.hide()
-        # self.Food3.clicked.connect(self.Food3Clicked)
-        #
-        # self.Food4 = QtWidgets.QPushButton(testDictionary["Food 4"]["Name"], self)
-        # self.Food4.setMinimumSize(130,130)
-        # self.Food4.move(500,500)
-        # self.Food4.setObjectName("Food 4")
-        # self.Food4.hide()
-        # self.Food4.clicked.connect(self.Food4Clicked)
-        #
-        # self.Food5 = QtWidgets.QPushButton(testDictionary["Food 5"]["Name"], self)
-        # self.Food5.setMinimumSize(130,130)
-        # self.Food5.setObjectName("Food 5")
-        # self.Food5.move(600,500)
-        # self.Food5.hide()
-        # self.Food5.clicked.connect(self.Food5Clicked)
-        #
-        # self.Food6 = QtWidgets.QPushButton(testDictionary["Food 6"]["Name"], self)
-        # self.Food6.setMinimumSize(130,130)
-        # self.Food6.setObjectName("Food 6")
-        # self.Food6.move(700,500)
-        # self.Food6.hide()
-        # self.Food6.clicked.connect(self.Food6Clicked)
-        #
-        # self.Food7 = QtWidgets.QPushButton(testDictionary["Food 7"]["Name"], self)
-        # self.Food7.setMinimumSize(130,130)
-        # self.Food7.setObjectName("Food 7")
-        # self.Food7.move(800,500)
-        # self.Food7.hide()
-        # self.Food7.clicked.connect(self.Food7Clicked)
-
-
-
         self.Food0 = QtWidgets.QPushButton("Food0", self)
         self.Food0.setMinimumSize(130,130)
         self.Food0.move(30,200)
@@ -202,14 +138,12 @@ class TestUI(QtWidgets.QWidget):
         self.Food1.setObjectName("Food 1")
         self.Food1.hide()
         self.Food1.clicked.connect(self.Food1Clicked)
-
         self.Food2 = QtWidgets.QPushButton("Food2", self)
         self.Food2.setMinimumSize(130,130)
         self.Food2.move(350,200)
         self.Food2.setObjectName("Food 2")
         self.Food2.hide()
         self.Food2.clicked.connect(self.Food2Clicked)
-
         self.Food3 = QtWidgets.QPushButton("Food3", self)
         self.Food3.setMinimumSize(130,130)
         self.Food3.move(510,200)
@@ -271,7 +205,6 @@ class TestUI(QtWidgets.QWidget):
         self.show()
     #Code for buttons warning terrible code ahead
 
-
     def Veges_Clicked(self):
         for i in range(len(self.FoodsDictionary)):
             self.FoodsDictionary[i].hide()
@@ -281,8 +214,6 @@ class TestUI(QtWidgets.QWidget):
             self.FoodsDictionary[i].setObjectName(testDictionary["Vegetable"][VegiesList[i]]["ObjectName"])
         self.CurrentFoodType = "Vegetable"
 
-        #self.Cart.append("This is a test")
-        #self.Food1.show()
         #self.Cart.append(random.randint(0, 10000)) This crashed it
         self.show()
     def Milk_Clicked(self):
@@ -294,8 +225,6 @@ class TestUI(QtWidgets.QWidget):
             self.FoodsDictionary[i].show()
             self.FoodsDictionary[i].setObjectName(testDictionary["Milk Product"][MilkProductsList[i]]["ObjectName"])
         self.CurrentFoodType = "Milk Product"
-        #self.Cart.append("This is a test")
-        #self.Food1.show()
         #self.Cart.append(random.randint(0, 10000)) This crashed it
         self.show()
     def Nuts_Clicked(self):
@@ -306,8 +235,6 @@ class TestUI(QtWidgets.QWidget):
             self.FoodsDictionary[i].show()
             self.FoodsDictionary[i].setObjectName(testDictionary["Nuts"][NutsList[i]]["ObjectName"])
         self.CurrentFoodType = "Nuts"
-        #self.Cart.append("This is a test")
-        #self.Food1.show()
         #self.Cart.append(random.randint(0, 10000)) This crashed it
         self.show()
     def Jams_Clicked(self):
@@ -318,8 +245,6 @@ class TestUI(QtWidgets.QWidget):
             self.FoodsDictionary[i].show()
             self.FoodsDictionary[i].setObjectName(testDictionary["Jam"][JamsList[i]]["ObjectName"])
         self.CurrentFoodType = "Jam"
-        #self.Cart.append("This is a test")
-        #self.Food1.show()
         #self.Cart.append(random.randint(0, 10000)) This crashed it
         self.show()
     def Juices_Clicked(self):
@@ -341,11 +266,9 @@ class TestUI(QtWidgets.QWidget):
         self.CurrentFoodType = "Fruit"
         self.show()
     def Food0Clicked(self):
-        #self.Cart.append(f"{testDictionary['Food 0']['Name']}: {testDictionary['Food 0']['Price']}")
-        #self.currentOrders.append()
         self.Cart.append(f'{testDictionary[self.CurrentFoodType][self.Food0.objectName()]["Name"]}: {testDictionary[self.CurrentFoodType][self.Food0.objectName()]["Price"]}')
         self.currentOrders.append(testDictionary[self.CurrentFoodType][self.Food0.objectName()]["RawPrice"])
-        #[self.CurrentFoodType][self.food0.objectName]["Name"]
+        self.CurrentCart.append(testDictionary[self.CurrentFoodType][self.Food0.objectName()]["RawPrice"])
         total = 0
         for item in self.currentOrders:
             total += item
@@ -439,9 +362,6 @@ class TestUI(QtWidgets.QWidget):
             total += item
             self.Total.setText(f"Total: ${total}")
 
-
-
-
     def Clear_Clicked(self):
         self.Cart.setText("")
         self.currentOrders = []
@@ -449,10 +369,9 @@ class TestUI(QtWidgets.QWidget):
     def CheckOut_Clicked(self):
         total = 0
         for item in self.currentOrders:
-            total += testDictionary[item]["RawPrice"]
+            total += item
         print(total)
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     main_window = TestUI()
     app.exec_()
-
